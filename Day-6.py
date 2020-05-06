4bf5763b34266972dace0e7d726c36ab4333a6ad
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-06 23:00:10 +0530

    Update Day-6.py

--- Day-6.py
+++ Day-6.py
@@ -1,3 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         print max(set(nums), key = nums.count)
+#or we can return the mode of the given list
+from statistics import mode
+class Solution:
+    def majorityelement(self,nums: List[int]) -> int:
+        return(mode(nums))
