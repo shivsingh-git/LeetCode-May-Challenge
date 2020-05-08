5904051b5b637e9440531f375a03c83bf81f19da
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-08 23:54:42 +0530

    Update Day-4.py

--- Day-4.py
+++ Day-4.py
@@ -1,4 +1,4 @@
-#Number Complement
+#Number Complement:
 #Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
 #Example 1
 #Input: 5
