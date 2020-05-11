4d90546859c5de0038ad45ba137ba814cbd47844
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-11 23:13:11 +0530

    Update Day-9.py

--- Day-9.py
+++ Day-9.py
@@ -4,7 +4,5 @@ class Solution:
         if num<0:
             return false
         else:
-            sqr=math.sqrt(num)
-            return((sqr - math.floor(sqr)) == 0)
-            
-        
+            sqr=math.sqrt(num)                                      #square root of that
+            return((sqr - math.floor(sqr)) == 0)                    #if it eqaul to zero subtracted from the floor of the number then true
