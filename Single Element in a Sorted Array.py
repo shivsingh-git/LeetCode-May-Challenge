39b030da7fb246b911dcfc5a76feca1b8cc915f3
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-14 21:32:39 +0530

    Update Single Element in a Sorted Array.py

--- Single Element in a Sorted Array.py
+++ Single Element in a Sorted Array.py
@@ -1,10 +1,10 @@
 #its a binary search approach
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        left, right = 0, len(nums)-1
+        left, right = 0, len(nums)-1                                    #creating the required sets of prelimenry conditions(left is frst node right is the last node)
         while left < right:
-            mid = int((left + right)/2)
-            if (mid % 2 == 1 and nums[mid - 1] == nums[mid]) or (mid%2 == 0 and nums[mid] == nums[mid + 1]):
+            mid = int((left + right)/2)                                 #finding the mid term of the list
+            if (mid % 2 == 1 and nums[mid - 1] == nums[mid]) or (mid%2 == 0 and nums[mid] == nums[mid + 1]):        #if the mid condition is satisfying the given set of sls
                 left = mid + 1
             else:
                 right = mid
